e90ca65fbed33cdff7b4a5e50b032dab7a8e4d14
unknown <mvickymosafan@gmail.com>
2024-11-13 18:29:49 +0700

    eleventh commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
-import { AnimatedLoginComponent } from "@/components/animated-login"
+import Component from "@/components/animated-login"
 
 export default function LoginPage() {
-  return <AnimatedLoginComponent />
+  return <Component />
 } 
